e7e88da88424b73ccf0e47510b9f4c55c24c6b80
ashutosh2287 <146572961+ashutosh2287@users.noreply.github.com>
2025-04-11 16:13:10 +0530

    initial commit
    Signed-off-by: ashutosh2287 <146572961+ashutosh2287@users.noreply.github.com>

---
+++ app/src/main/java/com/chatterbox/easychat/model/UserModel.java
@@ -0,0 +1,45 @@
+package com.chatterbox.easychat.model;
+
+import com.google.firebase.Timestamp;
+
+public class UserModel {
+    private String phone;
+    private String username;
+    private Timestamp createdTimestamp;
+
+    public UserModel() {
+    }
+
+    public UserModel(String phone, String username, Timestamp createdTimestamp) {
+        this.phone = phone;
+        this.username = username;
+        this.createdTimestamp = createdTimestamp;
+    }
+
+    public UserModel(String phoneNumber, String username, Timestamp now, String s) {
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Timestamp getCreatedTimestamp() {
+        return createdTimestamp;
+    }
+
+    public void setCreatedTimestamp(Timestamp createdTimestamp) {
+        this.createdTimestamp = createdTimestamp;
+    }
+}
